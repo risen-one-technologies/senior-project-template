9eb259d2b9f67be83c6f60ff579ddbc0fa4ab32f
Phil Rojas-Zdjelar <148258261+philrojaszdjelar@users.noreply.github.com>
2024-03-26 19:52:01 -0500

    Add debugging Nav Bar CSS and login page HTML and initial CSS styling

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { HomeComponent } from './home/home.component';
 import { AccountComponent } from './account/account.component';
 import { FormComponent } from './form/form.component';
 import { LoginComponent } from './login/login.component';
+import { NgModel } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -10,3 +12,13 @@ export const routes: Routes = [
     { path: 'form', component: FormComponent},
     { path: 'login', component: LoginComponent},
   ];
+
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+    providers: []
+  })
+
+  export class AppRoutingModule{
+    
+  }
