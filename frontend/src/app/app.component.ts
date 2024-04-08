f0541d30a794f0971282d8e5a62a9f9d136b9698
EvanPerlinger <111769977+EvanPerlinger@users.noreply.github.com>
2024-04-08 11:54:08 -0500

    Fixed Error in app.component.html

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, FormsModule
+    RouterOutlet, 
+    FormsModule, 
+    MatToolbarModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
